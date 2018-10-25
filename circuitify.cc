911d7325bad49726cc9cfb597754e82332fe89e2
Michael Straka <mstraka100@gmail.com>
2018-10-25 14:06:48 -0700

    wip

--- circuitify.cc
+++ circuitify.cc
@@ -22,34 +22,56 @@ using namespace std;
 typedef tuple<char, int> var_tuple;
 
 vector<struct mul> mul_data;
-map<string, Linear> varset;
+
 
 int mul_count = 0;
 int temp_count = 0;
 int bit_count = 0;
 mpz_class mod = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141_mpz;
 
-regex var_re = regex('[A-Za-z_][0-9a-zA-Z_]*');
-regex secret_re = regex('#(-?[0-9]+)');
-regex num_re = regex('[0-9]+');
+regex var_re = regex("[A-Za-z_][0-9a-zA-Z_]*");
+regex secret_re = regex("#(-?[0-9]+)");
+regex num_re = regex("[0-9]+");
 
 class Vars {
+
+private:
+	int var_offset(char type) {
+		if (type == 'L')
+			return 0;
+		else if (type == 'R') 
+			return 1;
+		else if (type == 'O')
+			return 2;
+		else if (type == 'T')
+			return 3;
+		else
+			throw invalid_argument("Invalid variable type");
+	}
+
 public:
 	map<int, mpz_class> var_map;
 
+
 	void add_var(char type, int idx, int val) {
-		int s;
-		if (type == 'L')
-			s = 0;
-		else if (type == 'R') 
-			s = 1;
-		else if (type == 'O')
-			s = 2;
-		else if (type == 'T')
-			s = 3;
-		else
-			throw invalid_argument("Invalid variable type");
+		int s = var_offset(type);
 		var_map[4*idx + s] = val;
+	}
+
+	bool has_var(char type, int idx) {
+		int s = var_offset(type);
+		int key = 4*idx + s;
+		return var_map.count(key) != 0;
+	}
+
+	mpz_class get_var(char type, int idx) {
+		int s = var_offset(type);
+		int key = 4*idx + s;
+		return var_map[key];
+	}
+
+	int num_vars() {
+		return var_map.size();
 	}
 
 	void add(Vars& other) {
@@ -100,6 +122,18 @@ class Linear {
 		vars.add_var(type, idx, val);
 	}
 
+	bool has_var(char type, int idx) {
+		return vars.has_var(type, idx);
+	}
+
+	int num_vars() {
+		return vars.num_vars();
+	}
+
+	mpz_class get_var(char type, int idx) {
+		return vars.get_var(type, idx);
+	}
+
 	void add(Linear& other) {
 		val += other.val;
 		vars.add(other.vars);
@@ -122,6 +156,7 @@ class Linear {
 };
 
 vector<Linear> eqs;
+map<string, Linear> varset;
 
 struct mul {
 	mpz_class l;
@@ -272,19 +307,18 @@ bool split_expr_binary(string& s, vector<string> ops, struct expr& result) {
 	return false;
 }
 
-
 Linear parse_expression(string s) {
 	s = clean_expr(s);
 	bool split;
 	if (s == "")
 		throw invalid_argument("Empty expresion");
 	struct expr sp;
-	vector<string> delim = {"^"}
+	vector<string> delim = {"^"};
 	split = split_expr_binary(s, delim, sp);
 	if (split) {
 		Linear left = parse_expression(sp.l);
 		Linear right = parse_expression(sp.r);
-		Linear ret = new_xor(left, right)
+		Linear ret = new_xor(left, right);
 		// TODO: assert result correct
 		return ret;
 	}
@@ -294,17 +328,17 @@ Linear parse_expression(string s) {
 		Linear left = parse_expression(sp.l);
 		Linear right = parse_expression(sp.r);
 		if (sp.op == "+")
-			sp.l.add(sp.r);
+			left.add(right);
 		else
-			sp.l.sub(sp.r)
-		return sp.l;
+			left.sub(right);
+		return left;
 	}
 	delim = {"*", "/"};
 	split = split_expr_binary(s, delim, sp);
 	if (split) {
 		Linear left = parse_expression(sp.l);
 		Linear right = parse_expression(sp.r);
-		if (op == "*") {
+		if (sp.op == "*") {
 			Linear ret = new_multiplication(left, right);
 			return ret;
 		} else {
@@ -318,13 +352,13 @@ Linear parse_expression(string s) {
 		Linear tmp2 = ret;
 		Linear one;
 		new_const(1, one);
-		temp2.sub(one);
+		tmp2.sub(one);
 		new_multiplication(tmp, tmp2, false);
 		bit_count += 1;
 		return ret;
 	}
 	if (s[0] == '-') {
-		Linear ret = parse_expression(s.substr(1))
+		Linear ret = parse_expression(s.substr(1));
 		ret.mul(mod-1);
 	}
 	if (regex_match(s, var_re)) {
@@ -333,20 +367,49 @@ Linear parse_expression(string s) {
 		else
 			throw invalid_argument("Variable not defined");
 	}
-	
+	if (regex_match(s, secret_re)) {
+		Linear ret;
+		int val = stoi(s.substr(1));
+		mpz_class mpz_val = val;
+		new_temp(mpz_val, ret);
+		return ret;
+	}
+	if (regex_match(s, num_re)) {
+		Linear ret;
+		int val = stoi(s);
+		mpz_class mpz_val = val;
+		new_const(mpz_val, ret);
+		return ret;
+	}
+	throw invalid_argument("Cannot parse expression");
 }
 
-/*void parse_expressions(string s) {
-	sruct expr sp;
+vector<Linear> parse_expressions(string s) {
+	struct expr sp;
 	vector<string> delim = {","};
-	bool split = split_expr_binary(s, delim);
+	bool split = split_expr_binary(s, delim, sp);
 	if (split) {
-		return parse_expressions
+		vector<Linear> l = parse_expressions(sp.l);
+		Linear r = parse_expression(sp.r);
+		l.push_back(r);
+		return l;
 	}
-}*/
+	Linear ret_val = parse_expression(s);
+	vector<Linear> ret;
+	ret.push_back(ret_val);
+	return ret;
+}
 
 
 //void split_by_delimiter(constconst string delim, )
+
+bool all_const(vector<Linear> v) {
+	for (int i = 0; i < v.size(); i++) {
+		if (!v[i].is_const)
+			return false;
+	}
+	return true;
+}
 
 typedef boost::tokenizer<boost::char_separator<char>> tokenizer;
 void parse_statement(string& s) {
@@ -361,16 +424,116 @@ void parse_statement(string& s) {
 		string left = sp.l;
 		string op = sp.op;
 		string right = sp.r;
+
 		if (op == ":=") {
+			vector<string> bits;
 			//TODO: Abstract out splitting string
 			boost::char_separator<char> sep{","};
   			tokenizer tok{left, sep};
-			vector<string> bits;
 			for (const auto &b : tok)
 				bits.push_back(b);
+			//TODO: Check bit names are valid
+			Linear val = parse_expression(right);
+			//TODO: assert bits length <= 256
+			vector<Linear> bitvars;
+			if (val.is_const) {
+				for (int i = 0;i < bits.size(); i++) {
+					Linear l;
+					mpz_class v = (val.val >> i) & 1;
+					new_const(v,l);
+					bitvars.push_back(l);
+				} 
+			} else {
+				for (int i = 0; i < bits.size(); i++) {
+					Linear l;
+					mpz_class v = (val.val >> i) & 1;
+					new_temp(v,l);
+					bitvars.push_back(l);
+					Linear tmp = l;
+					Linear tmp2 = l;
+					Linear one;
+					new_const(1, one);
+					tmp2.sub(one);
+					Linear eq = new_multiplication(tmp, tmp2);
+					eqs.push_back(eq);
+					tmp = l;
+					tmp.mul(1 << i);
+					val.sub(tmp);
+				}
+			}
+			for (int i = 0; i < bits.size(); i++) {
+				varset[bits[i]] = bitvars[i];
+			}
+		} else if (op == "=:") {
+			vector<Linear> bits;
+			bits = parse_expressions(right);
+			// TODO: Check varible name on left valid, len of bits <=256
+			mpz_class real = 0;
+			for (int i = 0; i < bits.size(); i++) {
+				//TODO: Assert bit either 0 or 1
+				real = real + bits[i].val * (1 << i);
+				if (all_const(bits)) {
+					Linear val;
+					new_const(real, val);
+					varset[left] = val;
+				} else {
+					Linear val;
+					new_temp(real, val);
+					varset[left] = val;
+					for (int i = 0; i < bits.size(); i++) {
+						Linear tmp = bits[i];
+						tmp.mul(1 << i);
+						val.sub(tmp);
+					}
+					eqs.push_back(val);
+				}
+			}
+		} else if (op == "=") {
+			//TODO: check left is proper variable name
+			Linear ex = parse_expression(right);
+			varset[left] = ex;
+		} else if (op == "==") {
+			Linear l = parse_expression(left);
+			Linear r = parse_expression(right);
+			l.sub(r);
+			//TODO: check l.val == r.val
+			eqs.push_back(l);
 		}
-		//TODO: Check bit names are valid
-		// val = parse_expression(right)
+	}
+}
+
+
+void pivot_variable(vector<Linear>& eqs, char type, int idx, bool eliminate = false) {
+	int c = 0;
+	int cc = 0;
+	int low = -1;
+	Linear leq;
+	vector<int> vec;
+	for (int i = 0; i < eqs.size(); i++) {
+		if (eqs[i].has_var(type, idx)) {
+			vec.push_back(i);
+			cc += 1;
+			if (low == -1 || c > eqs[i].num_vars()) {
+				low = i;
+				c = eqs[i].num_vars();
+				Linear tmp = eqs[i];
+				tmp.mul(modinv(eqs[i].get_var(type, idx)));
+				leq = eqs[i];
+				break;
+			}
+		}
+	}
+	if (cc > 1) {
+		for (auto i: vec) {
+			if (i != low) {
+				Linear tmp = leq;
+				tmp.mul(eqs[i].get_var(type, idx));
+				eqs[i].sub(tmp);
+			}
+		}
+	}
+	if (eliminate and cc > 0) {
+		eqs.erase(eqs.begin()+low);
 	}
 }
 
@@ -378,10 +541,12 @@ void parse_statement(string& s) {
 int main() {
 //  printf("%d\n", modinv(5));
   
-  mpz_class n;
+/*  mpz_class n;
   n = 0;
   printf("n = ");
   cout << n;
-  printf("\n");
+  printf("\n");*/
+
+
   return 0;
 }
